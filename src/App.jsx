3b0906f29971933b35d93f21fa54d70aaa820de2
im3t <trungtintran0711@gmail.com>
2025-07-28 11:10:31 +0900

    configure auth, data, storage resources, start sandbox, connect backend, build frontend allowing CRUD

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
+import { Amplify } from "aws-amplify";
+import outputs from "../amplify_outputs.json";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+
+// Cấu hình Amplify với backend (User Pool, API, Storage)
+Amplify.configure(outputs);
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-        <h1>Hello from Amplify</h1>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div className="App">
+          <header className="App-header">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+            <h1>Hello {user?.username}</h1>
+            <button onClick={signOut}>Sign out</button>
+          </header>
+        </div>
+      )}
+    </Authenticator>
   );
 }
+
 export default App;
